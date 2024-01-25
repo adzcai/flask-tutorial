c4415aa0244ebef67df3e2b24ab46a54a552e5c2
Alexander Cai <alexandercai@outlook.com>
2024-01-25 16:21:04 -0500

    finish tutorial without styling

--- app.py
+++ app.py
@@ -1,10 +1,50 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
-# an example of a *route*, a concept for organizing server endpoints
+# concept: *databases* for persisting data
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
+@app.route('/add', methods=['POST'])
+def add():
+    # concept: *requests* for handling client requests
+    title = request.form.get('title')
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/update/<int:todo_id>')
+def update(todo_id):
+    todo = Todo.query.get(todo_id)
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/delete/<int:todo_id>')
+def delete(todo_id):
+    todo = Todo.query.get(todo_id)
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+# concept: *routes* for organizing server endpoints
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    todos = Todo.query.all()
+    return render_template('base.html', todos=todos)
 
 if __name__ == '__main__':
-    app.run()
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
+
